00cdf74347d1615a2c87522c29b0a8af75133799
Rushikesh <40839630+boring-personality@users.noreply.github.com>
2024-08-16 13:01:43 +0530

    added command line parameters (#1)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::fs::OpenOptions;
-use std::io::Write;
 use std::fs::File;
 use std::fs::read_to_string;
+use std::io::Write;
+use std::env;
+use std::path::Path;
+
+static FILE_NAME: &str = "TaskList.txt";
 
 pub fn display_menu() {
     println!("1. Add a new task");
@@ -27,46 +31,58 @@ pub fn file_write(task: &mut String) {
                 .append(true)
                 .write(true)
                 .create(true)
-                .open("TaskList.txt")
+                .open(FILE_NAME)
                 .expect("Could not add task!!");
     file.write_all(task.as_bytes()).expect("Write failed");
+    let _ = file.write_all(b"\n");
     println!("Task {} added successfully", task);
 } 
 
+pub fn clear_todo() {
+    std::fs::remove_file(FILE_NAME).expect("Could not clear tasks");
+    println!("To-Do pile cleared!!!");
+}
+
 pub fn file_read() {
     // For showing all the tasks which are added
-    // let mut file = OpenOptions::new()
-    //             .read(true)
-    //             .open("TaskList.txt")
-    //             .expect("Could not show tasks!!");
-    println!("--------------------------------------------------------------------------------");
-    for (i, line) in read_to_string("TaskList.txt").unwrap().lines().enumerate() {
-        println!("{}) {}", i+1, line);
+    if Path::new(FILE_NAME).is_file() {
+        println!("--------------------------------------------------------------------------------");
+        for (i, line) in read_to_string(FILE_NAME).unwrap().lines().enumerate() {
+            println!("{}) {}", i+1, line);
+        }
+        println!("--------------------------------------------------------------------------------");
     }
-    println!("--------------------------------------------------------------------------------");
+    else {
+        println!("No tasks in To-Do");
+    }
 }
 
 pub fn delete_task(task_no: i32) {
     // remove the task from file take the task number as input
     let mut content: Vec<String> = Vec::new();
-    for (i, line) in read_to_string("TaskList.txt").unwrap().lines().enumerate() {
-        if i+1 == task_no as usize {
-            continue
+    if Path::new(FILE_NAME).is_file() {
+        for (i, line) in read_to_string(FILE_NAME).unwrap().lines().enumerate() {
+            if i+1 == task_no as usize {
+                continue
+            }
+            content.push(line.to_string());
         }
-        content.push(line.to_string());
+        let mut file = File::create(FILE_NAME).expect("Could not open file");
+        for cont in content {
+            file.write_all(cont.as_bytes()).expect("Write Failed");
+            let _ = file.write_all(b"\n");
+        }
+        println!("Deleted task {} successfully", task_no);
     }
-    let mut file = File::create("TaskList.txt").expect("Could not open file");
-    for cont in content {
-        file.write_all(cont.as_bytes()).expect("Write Failed");
-        let _ = file.write_all(b"\n");
+    else {
+        println!("No tasks in To-Do to remove")
     }
-    println!("Deleted task {} successfully", task_no);
 }
 
 pub fn mark_complete(task_no: i32) {
     // strikethrough the task which is completed
     let mut content: Vec<String> = Vec::new();
-    for (i, line) in read_to_string("TaskList.txt").unwrap().lines().enumerate() {
+    for (i, line) in read_to_string(FILE_NAME).unwrap().lines().enumerate() {
         if i+1 == task_no as usize {
             content.push(strikethrough(&mut line.to_string()));
         }
@@ -74,7 +90,7 @@ pub fn mark_complete(task_no: i32) {
             content.push(line.to_string());
         }
     }
-    let mut file = File::create("TaskList.txt").expect("Could not open file");
+    let mut file = File::create(FILE_NAME).expect("Could not open file");
     for cont in content {
         file.write_all(cont.as_bytes()).expect("Write Failed");
         let _ = file.write_all(b"\n");
@@ -82,7 +98,7 @@ pub fn mark_complete(task_no: i32) {
     println!("Completed task {} successfully", task_no);
 }
 
-fn main() {
+pub fn interactive() {
     println!("Welcome to the TO-DO list app!!!");
     loop {
         let mut input: String = String::new();
@@ -117,6 +133,61 @@ fn main() {
             }
             Err(_) | _ => { println!("Invalid input!!Please try again"); }
         }
-        println!("This is the input: {}", input);
+    }
+}
+
+fn main() {
+    // Try to take the inputs from command line rather than having interactive application
+    // Examples could be, "ToDoList add <task name>", "ToDoList show", "ToDoList remove <task no>", ToDoList complete <task no>
+    let mut args = env::args().skip(1);
+    
+    while let Some(arg) = args.next() {
+        match &arg[..] {
+            // "-h" | "help" => {
+            //     help();
+            // }
+            // "version" => {
+            //     println!("{} {}", prog().unwrap_or_default(), VERSION);
+            // }
+            "-a" | "add" => {
+                if let Some(task_name) = args.next() {
+                    let mut task = task_name;
+                    file_write(&mut task);
+                }
+                else {
+                    println!("No task specified for add");
+                }
+            }
+            "-r" | "remove" => {
+                if let Some(task_no) = args.next() {
+                    let task = task_no;
+                    delete_task(task.trim().parse().unwrap());
+                }
+                else {
+                    println!("No task number provided to remove");
+                }
+            }
+            "-c" | "complete" => {
+                if let Some(task_no) = args.next() {
+                    let task = task_no;
+                    mark_complete(task.trim().parse().unwrap());
+                }
+                else {
+                    println!("No task number provided to complete");
+                }
+            }
+            "-s" | "show" => {
+                file_read();
+            }
+            "-cl" | "clear" => {
+                clear_todo();
+            }
+            "-i" | "interactive" => {
+                interactive();
+            }
+            _ => {
+                println!("Unknown argument");
+            }
+        }
     }
 }
